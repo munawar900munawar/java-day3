f80b73fe5e7f8fd6b25302f39d8339c785cfed40
munawar900munawar <shaikskarishma9@gmail.com>
2024-12-04 11:05:34 +0530

    Add files via upload

---
+++ rightshift.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class rightshift{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=a/(int)Math.pow(2,b);
+        System.out.println(c);
+    }
+}
